afe01d411b2f1594db27866b3dd143d91d0216eb
Ian Douglas Scott <ian@iandouglasscott.com>
2018-01-13 18:30:32 -0800

    Do not have a main.rs; but include an example

---
+++ examples/print_tree.rs
@@ -0,0 +1,42 @@
+extern crate iso9660;
+
+use std::{env, process};
+
+use iso9660::{ISO9660, ISODirectory, DirectoryEntry};
+
+fn main() {
+    let args = env::args();
+
+    if args.len() != 2 {
+        eprintln!("Requires 1 argument.");
+        process::exit(1);
+    }
+
+    let path = env::args().nth(1).unwrap();
+    let fs = ISO9660::new(path).unwrap();
+
+    print_tree(&fs.root, 0);
+}
+
+fn print_tree(dir: &ISODirectory, level: u32) {
+    for entry in dir.contents().unwrap() {
+        match entry {
+            DirectoryEntry::Directory(dir) => {
+                if dir.identifier == "\0" || dir.identifier == "\u{1}" {
+                    continue;
+                }
+                for i in 0..level {
+                    print!("  ");
+                }
+                println!("* {}", dir.identifier);
+                print_tree(&dir, level+1);
+            },
+            DirectoryEntry::File(file) => {
+                for i in 0..level {
+                    print!("  ");
+                }
+                println!("- {}", file.identifier);
+            }
+        }
+    }
+}
