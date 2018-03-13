b630d8a2b9f8f5f36887d4fcaeaf63d564e6e355
Ian Douglas Scott <ian@iandouglasscott.com>
2018-03-12 22:01:05 -0700

    print_tree example: support second argument with path to directory

--- examples/print_tree.rs
+++ examples/print_tree.rs
@@ -7,15 +7,34 @@ use iso9660::{ISO9660, ISODirectory, DirectoryEntry};
 fn main() {
     let args = env::args();
 
-    if args.len() != 2 {
-        eprintln!("Requires 1 argument.");
+    if args.len() < 2 || args.len() > 3 {
+        eprintln!("Requires 1 or 2 arguments.");
         process::exit(1);
     }
 
-    let path = env::args().nth(1).unwrap();
+    let mut args = env::args().skip(1);
+    let path = args.next().unwrap();
+    let dirpath = args.next();
+
     let fs = ISO9660::new(path).unwrap();
 
-    print_tree(&fs.root, 0);
+    if let Some(dirpath) = dirpath {
+        match fs.open(&dirpath).unwrap() {
+            Some(DirectoryEntry::Directory(dir)) => {
+                print_tree(&dir, 0);
+            }
+            Some(DirectoryEntry::File(_)) => {
+                eprintln!("'{}' is not a directory", dirpath);
+                process::exit(1);
+            }
+            None => {
+                eprintln!("'{}' does not exist", dirpath);
+                process::exit(1);
+            }
+        }
+    } else {
+        print_tree(&fs.root, 0);
+    }
 }
 
 fn print_tree(dir: &ISODirectory, level: u32) {
